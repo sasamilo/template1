4878f337cffe1abdd71db23b240d8de309afc4c9
Sasa Milo <sasakampic@gmail.com>
2020-09-09 15:47:38 +0200

    layout fixes

--- static/app.js
+++ static/app.js
@@ -19,3 +19,42 @@ const navSlide = () => {
 }
 
 navSlide();
+
+const sections = document.querySelectorAll('section');
+const bubble = document.querySelector('bubble');
+const gradients = [
+    "linear-gradient(to right top, #fafafa, #dadada)",
+    "linear-gradient(to right top, #fafafa, #dadada)",
+    "linear-gradient(to right top, #fafafa, #dadada)"
+];
+
+const options = {
+    threshold: 0.7
+};
+
+let observer = new IntersectionObserver(navCheck, options);
+
+function navCheck(entries) {
+    entries.forEach(entry => {
+        const className = entry.target.className;
+        const activeAnchor = document.querySelector(`[data-page=$className]`);
+        const coords = activeAnchor.getBoundingClientRect();
+        const directions = {
+            height: coords.height,
+            width: coords.width,
+            top: coords.top,
+            left: coords.left,
+        };
+        if (entry.isIntersecting) {
+            bubble.style.setProperty("height", `{directions.height}px`);
+            bubble.style.setProperty("top", `{directions.top}px`);
+            bubble.style.setProperty("width", `{directions.width}px`);
+            bubble.style.setProperty("left", `{directions.left}px`);
+            bubble.style.background = gradients[gradientIndex];
+        }
+    });
+}
+
+sections.forEach(section => {
+    observer.observe(section);
+});
